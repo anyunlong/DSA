fd150f7989d76f3adbc76c6f80108c3b77a3bfc1
Yunlong An <anyl@rd.netease.com>
2019-09-23 10:48:02 +0800

    237-删除链表中的节点

---
+++ leetcode/237-删除链表中的节点.cpp
@@ -0,0 +1,9 @@
+// struct ListNode {
+//     int val;
+//     struct ListNode *next;
+// };
+
+void deleteNode(struct ListNode* node) {
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
